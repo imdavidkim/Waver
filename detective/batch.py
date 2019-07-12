e7b20a5f22535b21aeacaac56761002a0f86d5e9
imdavidkim <35369385+imdavidkim@users.noreply.github.com>
2019-07-12 19:39:22 +0900

    Update batch.py

--- detective/batch.py
+++ detective/batch.py
@@ -6,11 +6,15 @@ from detective.crawler import getStockInfo
 from detective.detector import new_find_hidden_pearl
 from detective.detector import get_high_ranked_stock
 from detective.detector import messeage_to_telegram
+from detective.kofiabond import get_list_day
 
 if __name__ == '__main__':
+    get_list_day()
     getStockInfo()
     run_info = [101, 200]
     agents = 2
     # chunksize = 3
     with Pool(processes=agents) as pool:
         result = pool.map(getFinanceData, run_info)
+    new_find_hidden_pearl()
+    messeage_to_telegram(get_high_ranked_stock())
