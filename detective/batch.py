96cfdbf480b4d11768d774589b044285cd9f82e5
imdavidkim <35369385+imdavidkim@users.noreply.github.com>
2019-10-15 17:17:27 +0900

    Update batch.py

--- detective/batch.py
+++ detective/batch.py
@@ -5,11 +5,16 @@ from detective.fnguide_collector import getFinanceData
 from detective.crawler import getStockInfo
 from detective.detector import new_find_hidden_pearl
 from detective.detector import get_high_ranked_stock
-from detective.detector import messeage_to_telegram
+from detective.messenger import messeage_to_telegram
 from detective.kofiabond import get_list_day
+from detective.fred import make_graph as fred_make_graph
+from detective.kofia import make_graph as kofia_make_graph
 
 if __name__ == '__main__':
-    get_list_day()
+    # get_list_day(None)
+    # # get_list_day('20191001')
+    kofia_make_graph()
+    fred_make_graph()
     getStockInfo()
     run_info = [101, 200]
     agents = 2
