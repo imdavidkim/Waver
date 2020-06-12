1ffd2a3bb23d4894c887d36616ecda7069a850f9
imdavidkim <35369385+imdavidkim@users.noreply.github.com>
2020-06-12 15:18:28 +0900

    Update test.py

--- detective/test.py
+++ detective/test.py
@@ -7,6 +7,8 @@ import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import numpy as np
+
 
 def request(driver):
     s = requests.Session()
@@ -17,40 +19,52 @@ def request(driver):
 
 if __name__ == '__main__':
 
-    url = 'http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp'
-    data = {
-            'pGB': 1,
-            'gicode': 'A007630',
-            'cID': '',
-            'MenuYn': 'Y',
-            'ReportGB': 'D',  # D: 연결, B: 별도
-            'NewMenuID': 101,
-            'stkGb': 701,
-        }
-
+    # url = 'http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp'
+    # data = {
+    #         'pGB': 1,
+    #         'gicode': 'A007630',
+    #         'cID': '',
+    #         'MenuYn': 'Y',
+    #         'ReportGB': 'D',  # D: 연결, B: 별도
+    #         'NewMenuID': 101,
+    #         'stkGb': 701,
+    #     }
+    #
     CHROMEDRIVER_PATH = r'D:\Waver\chromedriver_win32\chromedriver.exe'
     options = Options()
     options.headless = True
     driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
     # driver.implicitly_wait(3)
     req = request(driver)
-    d = json.dumps(data)
-    # driver.get('http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A003490&cID=&MenuYn=Y&ReportGB=D&NewMenuID=101&stkGb=701')
-    # print(driver.page_source)
-    res = req.post(url, data)
-    print(res.text)
-    # soup = BeautifulSoup(driver.page_source, 'lxml')
-    # print(soup)
+    # d = json.dumps(data)
+    # # driver.get('http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A003490&cID=&MenuYn=Y&ReportGB=D&NewMenuID=101&stkGb=701')
+    # # print(driver.page_source)
+    # # res = req.post(url, data)
+    for i in range(10):
+        en = ''
+        n = 5
+        en += ''.join(["{}".format(np.random.randint(0, 9)) for num in range(0, n)])
+        en += '06'
+        en += ''.join(["{}".format(np.random.randint(0, 9)) for num in range(0, n)])
+        en += '11'
+        url = 'http://compglobal.wisereport.co.kr/miraeassetdaewoo/company/get_snap_financial_summary?ticker=MSFT-US&freq_typ=A&en={}'.format(en)
+        print(url)
+        res = req.get(url)
+        print(res.text)
+    # # soup = BeautifulSoup(driver.page_source, 'lxml')
+    # # print(soup)
 
 
 
 
-
+    #
     # import telegram
-    # my_token = '577949495:AAFk3JWQjHlbJr2_AtZeonjqQS7buu8cYG4'
-    # my_token = '781845768:AAEG55_jbdDIDlmGXWHl8Ag2aDUg-YAA8fc'
+    # # my_token = '577949495:AAFk3JWQjHlbJr2_AtZeonjqQS7buu8cYG4'
+    # # my_token = '781845768:AAEG55_jbdDIDlmGXWHl8Ag2aDUg-YAA8fc'
+    # my_token = '866257502:AAH3zxEzlNT-venJnI-ZacJBwrnh2nxLsNk'
     # telegram.Bot(token=my_token)
     # bot = telegram.Bot(token=my_token)
+    #
     # updates = bot.getUpdates()
     # for u in updates:
     #     print(u.message)
