4f01a68016b19e1da2117c46977a2d2637f725ec
imdavidkim <35369385+imdavidkim@users.noreply.github.com>
2020-07-17 14:02:34 +0900

    Update messenger.py

--- detective/messenger.py
+++ detective/messenger.py
@@ -22,38 +22,85 @@
 #     print(u.message)
 
 
+def getConfig():
+    from datetime import datetime
+    import configparser
+    global daddy, ggmsg, chart, error, chat_id_kh, chat_id_km, yyyymmdd
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    daddy = config['TELEGRAM']['DADDY']
+    ggmsg = config['TELEGRAM']['GGMSG']
+    chart = config['TELEGRAM']['CHART']
+    error = config['TELEGRAM']['ERROR']
+    chat_id_kh = config['TELEGRAM']['CHAT_ID_KH']
+    chat_id_km = config['TELEGRAM']['CHAT_ID_KM']
+    yyyymmdd = str(datetime.now())[:10]
+
+
+def sendMessage(token, chatid, txt):
+    import telegram
+    bot = telegram.Bot(token=token)
+    print(txt)
+    bot.sendMessage(chat_id=chatid, text=txt)
+
+
+def sendImage(token, chatid, img_path):
+    import telegram
+    bot = telegram.Bot(token=token)
+    print(img_path)
+    if img_path is not None and img_path != '':
+        bot.sendPhoto(chat_id=chatid, photo=open(img_path, 'rb'))
+
+
 def messeage_to_telegram(txt):
     import time
-    import telegram
-    my_token = '577949495:AAFk3JWQjHlbJr2_AtZeonjqQS7buu8cYG4'
-    chat_id = '568559695'
-    bot = telegram.Bot(token=my_token)
-    print(txt)
+    DEBUG = False
     if txt is not None and txt != '':
-        bot.sendMessage(chat_id=chat_id, text=txt)
-    time.sleep(3)
-    my_token = '866257502:AAH3zxEzlNT-venJnI-ZacJBwrnh2nxLsNk'
-    chat_id = '869289245'
-    bot = telegram.Bot(token=my_token)
-    print(txt)
-    if txt is not None and txt != '':
-        bot.sendMessage(chat_id=chat_id, text=txt)
-
-
+        if not DEBUG:
+            msg = yyyymmdd + '\n' + txt
+            sendMessage(ggmsg, chat_id_kh, msg)
+            time.sleep(3)
+            sendMessage(daddy, chat_id_km, msg)
+        else:
+            msg = yyyymmdd + '\n' + txt
+            sendMessage(ggmsg, chat_id_kh, msg)
 
 def img_messeage_to_telegram(img_path):
     import time
+    DEBUG = False
+    if img_path is not None and img_path != '':
+        if not DEBUG:
+            sendMessage(chart, chat_id_kh, yyyymmdd)
+            sendImage(chart, chat_id_kh, img_path)
+            time.sleep(3)
+            sendMessage(daddy, chat_id_km, yyyymmdd)
+            sendImage(daddy, chat_id_km, img_path)
+        else:
+            sendMessage(chart, chat_id_kh, yyyymmdd)
+            sendImage(chart, chat_id_kh, img_path)
+
+
+def err_messeage_to_telegram(txt):
+    sendMessage(error, chat_id_kh, txt)
+
+
+def check_messenger_bot(token):
     import telegram
-    my_token = '577949495:AAFk3JWQjHlbJr2_AtZeonjqQS7buu8cYG4'
-    chat_id = '568559695'
+    my_token = '1267762976:AAFIwmUknc3jIP8vqH4qqmT78AYFUKs0Umw'
     bot = telegram.Bot(token=my_token)
-    print(img_path)
-    if img_path is not None and img_path != '':
-        bot.sendPhoto(chat_id=chat_id, photo=open(img_path, 'rb'))
-    time.sleep(3)
-    my_token = '866257502:AAH3zxEzlNT-venJnI-ZacJBwrnh2nxLsNk'
-    chat_id = '869289245'
-    bot = telegram.Bot(token=my_token)
-    print(img_path)
-    if img_path is not None and img_path != '':
-        bot.sendPhoto(chat_id=chat_id, photo=open(img_path, 'rb'))
+    a = bot.getUpdates()
+    for i in a:
+        print(i)
+
+
+def sendMessage(token, chatid, txt):
+    import telegram
+    bot = telegram.Bot(token)
+    bot.sendMessage(chat_id=chatid, text=txt)
+
+if __name__ == '__main__':
+    chart_messenger_token = '1267762976:AAFIwmUknc3jIP8vqH4qqmT78AYFUKs0Umw'
+    chat_target_id = '568559695'
+    message = '20200717 ChartInfo'
+    sendMessage(chart_messenger_token, chat_target_id, message)
+    # check_messenger_bot()
