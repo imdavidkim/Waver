ea30950f35ccfcc44dab6bb32b546bf16c3b3dff
imdavidkim <35369385+imdavidkim@users.noreply.github.com>
2020-08-14 11:51:10 +0900

    Update batch.py

--- detective/batch.py
+++ detective/batch.py
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from multiprocessing import Pool
 from detective.fnguide_collector import getFinanceData
 from detective.crawler import getStockInfo
-from detective.crawler import getSnP500StockInfo
-from detective.crawler import getNasdaq100StockInfo
+
 from detective.detector import new_find_hidden_pearl
 from detective.detector import get_high_ranked_stock
 from detective.detector import get_high_ranked_stock_with_closeprice
+
 from detective.messenger import messeage_to_telegram
 from detective.kofiabond import get_list_day
 from detective.fred import make_graph as fred_make_graph
 from detective.kofia import make_graph as kofia_make_graph
-import multiprocessing
 import multiprocessing.pool
-
-from multiprocessing import Pool
 
 
 class NoDaemonProcess(multiprocessing.Process):
@@ -33,23 +29,18 @@ class NonDaemonicPool(multiprocessing.pool.Pool):
 
 
 if __name__ == '__main__':
-    # getStockInfo()
-    # getSnP500StockInfo()
-    # getNasdaq100StockInfo()
-    # get_list_day(None)
-    # get_list_day('20200708')
-    # kofia_make_graph()
-    # fred_make_graph()
+    getStockInfo()
+    get_list_day(None)
+    kofia_make_graph()
+    fred_make_graph()
 
-    # run_info = [101, 200, 300]
-    # agents = 3
-    run_info = [300]
-    agents = 1
+    run_info = [101, 200]
+    agents = 2
 
-    # with Pool(processes=agents) as pool:
     with NonDaemonicPool(processes=agents) as pool:
         result = pool.map(getFinanceData, run_info)
-    # new_find_hidden_pearl()
-    # messeage_to_telegram(get_high_ranked_stock())
-    # get_high_ranked_stock_with_closeprice()
+    new_find_hidden_pearl()
+    messeage_to_telegram(get_high_ranked_stock())
+    get_high_ranked_stock_with_closeprice()
+
 
